f0c560166bb8ced801ef35e33498d33aa85faf5f
Dennis <thecvcoder@foxmail.com>
2024-05-21 16:28:27 +0800

    init commit

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+export const SITE_TITLE = 'Dreamy Coastline'
+export const SITE_DESC = `${SITE_TITLE} is rock band from China.`
+export const SITE_URL = "/"
+
+export const MEDIA_LINKS = [
+  { icon: "wyy", link: "/" },
+  { icon: "qqyy", link: "/" },
+  { icon: "spotify", link: "/" },
+  { icon: "twitter", link: "/" },
+  { icon: "instagram", link: "/" },
+];
---
+++ src/pages/discographies/once-in-a-blue-moon.md
@@ -0,0 +1,8 @@
+---
+title: Once in a blue moon
+desc: Once in a blue moon
+heroImage: /tracks/109951169219512461.jpg
+link: https://music.163.com/song?id=2100627235&userid=5076401671
+type: Single
+date: 2021/08/04
+---
